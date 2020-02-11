b1a10a6d33d379f8964f8cb8e0c02b31e4c344b8
hkt <komikansougou@gmail.com>
2020-02-12 00:03:46 +0900

    feat: RESAS API から都道府県を取得して表示

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
 import Template from '../components/templates/App';
 import { Prefecture } from '../types';
 
 const App: React.FC = () => {
-  const prefectures: Prefecture[] = [
-    {
-      prefName: '北海道',
-      prefCode: 0,
-    },
-    {
-      prefName: '青森県',
-      prefCode: 1,
-    },
-  ];
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      let resasPrefectures: Prefecture[] = [];
+
+      try {
+        resasPrefectures = await fetch(`${process.env.API}/prefectures`)
+          .then((r: any) => r.json())
+          .then((data: any): Prefecture[] => data.map((v: any): Prefecture => ({
+            prefName: v.prefName,
+            prefCode: v.PrefCode,
+          })));
+      } catch (_) {
+        alert('都道府県を取得できませんでした。通信環境をご確認ください。');
+        return;
+      }
+
+      setPrefectures(resasPrefectures);
+    })();
+  }, []);
 
   return (
     <Template prefectures={prefectures} />
