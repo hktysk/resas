dad1d181ffeca4db57007f62e0c8544bb527195c
hkt <komikansougou@gmail.com>
2020-02-11 23:35:38 +0900

    chore: TypeScript を使うので eslint の prop-types を無効化

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
   extends: "airbnb-typescript",
   parserOptions: {
     project: './tsconfig.json'
-  }
+  },
+  "overrides": [
+    {
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off"
+      }
+    }
+  ]
 }
